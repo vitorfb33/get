d524ea3f4dc1c7375a35e6db81be71d3cb10374c
VITOR FIALHO BERGAMI <vitorfb33@hotmail.com>
2023-11-08 22:17:52 -0300

    colocando os selos

--- get.js
+++ get.js
@@ -74,9 +74,9 @@ function calcular() {
 }
 
 function limpar() {
-    document.getElementById("imc").innerHTML = ""
-    document.getElementById("status").innerHTML = ""
-    document.getElementById("freq").innerHTML = ""
+    document.getElementById("imc").innerHTML = "IMC"
+    document.getElementById("status").innerHTML = "status"
+    document.getElementById("freq").innerHTML = "FCM"
     document.getElementById("grafico").src = "icons/reset.png"
-    document.getElementById("calorias").innerHTML = ""
+    document.getElementById("calorias").innerHTML = "calorias"
 }
